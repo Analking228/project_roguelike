12e3890b12c4d19fd453e4555fb16be4562976ef
analking228 <kronos5525@yandex.ru>
2022-04-17 20:30:03 +0300

    Settings window designder6 but useless

--- src/main/java/View/Frames/SettingsFrame.java
+++ src/main/java/View/Frames/SettingsFrame.java
@@ -2,29 +2,37 @@ package View.Frames;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SettingsFrame extends JFrame {
+public class SettingsFrame extends JFrame implements ActionListener {
+
+    JButton     enterBtn;
+    JTextField  sizeTF;
 
     public SettingsFrame(int screenSizeX, int screenSizeY, int insets) {
-        super("Set window size");
+        super("Main window size");
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.setLayout(null);
+        this.setLayout(new FlowLayout());
         this.setBackground(Color.lightGray);
         this.setResizable(false);
-        this.setSize(320, 50 + insets);
-        JTextField settingsTF = new JTextField();
-        settingsTF.setPreferredSize(new Dimension(250, 30));
-        settingsTF.setLocation(5, 10);
+        this.setSize(200, 50 + insets);
+        this.sizeTF = new JTextField();
+        this.sizeTF.setPreferredSize(new Dimension(100, 40));
         Font font = new Font(Font.SERIF, Font.PLAIN, 18);
-        settingsTF.setFont(font);
-        this.add(settingsTF);
-        JButton     enterBtn = new JButton("Enter");
-        enterBtn.setSize(new Dimension(50, 30));
-        enterBtn.setLocation(320 - 50 - 5, 10);
-        this.add(enterBtn);
-        this.setLocation(screenSizeX / 2 - 320 / 2, screenSizeY / 2 - 50);
+        this.sizeTF.setFont(font);
+        this.add(this.sizeTF);
+        this.enterBtn = new JButton("Enter");
+        this.enterBtn.setSize(new Dimension(60, 40));
+        this.add(this.enterBtn);
+        this.setLocation(screenSizeX / 2 - 200 / 2, screenSizeY / 2 - 50);
         this.setVisible(true);
         this.requestFocus();
         this.setFocusable(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
