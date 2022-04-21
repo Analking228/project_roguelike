7ac9f8e27cd1b236c8e30fe78c64c6b3f4895e57
analking228 <kronos5525@yandex.ru>
2022-04-21 15:10:30 +0300

    select/create hero panel moded

--- src/main/java/View/Panels/SelectHeroPanel.java
+++ src/main/java/View/Panels/SelectHeroPanel.java
@@ -12,10 +12,18 @@ public class            SelectHeroPanel extends JPanel {
         this.observer = o;
         String[]        createdHeroes = {"John the Nigger", "Pavel the Greatest", "John the Walker", "Pinus", "Pinus", "Pinus", "Pinus", "Pinus", "Pinus", "Pinus", "Pinus", "Pinus", "Pinus"};
         JList<String>   list = new JList<>(createdHeroes);
+        JList<String>   list2 = new JList<>(createdHeroes);
         JScrollPane     scrollPane = new JScrollPane(list);
-        JScrollPane     scrollPane2 = new JScrollPane(list);
+        JScrollPane     scrollPane2 = new JScrollPane(list2);
+        this.setLayout(new BorderLayout());
         this.add(scrollPane, BorderLayout.WEST);
         this.add(scrollPane2, BorderLayout.EAST);
+        JPanel          buttonPanel = new JPanel();
+        JButton         selectHeroBtn = new JButton("Select Hero");
+        JButton         createNewHeroBtn = new JButton("Create new Hero");
+        buttonPanel.add(selectHeroBtn);
+        buttonPanel.add(createNewHeroBtn);
+        this.add(buttonPanel, BorderLayout.SOUTH);
 
         this.setFocusable(true);
         this.requestFocusInWindow();
