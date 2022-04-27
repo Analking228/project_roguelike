bb4386be8a2455b2360f5ddb44182b23e33bccfc
analking228 <kronos5525@yandex.ru>
2022-04-27 18:06:41 +0300

    listeners on TFs

--- src/main/java/View/Panels/NewHeroPanel.java
+++ src/main/java/View/Panels/NewHeroPanel.java
@@ -23,8 +23,10 @@ public class            NewHeroPanel extends JPanel implements ActionListener {
 
         JPanel          namePanel = new JPanel(new GridLayout(2,2));
         this.nameTF = new JTextField("Enter Hero's name");
+        this.nameTF.addCaretListener(e -> clearTF(this.nameTF));
         JLabel          nameLbl = new JLabel("Name: ");
         this.titleTF = new JTextField("Enter Hero's title");
+        this.titleTF.addCaretListener(e -> clearTF(this.titleTF));
         JLabel          tittleLbl = new JLabel("Title: ");
         namePanel.add(nameLbl);
         namePanel.add(nameTF);
@@ -44,7 +46,7 @@ public class            NewHeroPanel extends JPanel implements ActionListener {
         c.gridwidth = 3;
         c.fill = GridBagConstraints.HORIZONTAL;
         this.add(this.classComboBox, c);
-        this.classDescTA = new JTextArea("");
+        this.classDescTA = new JTextArea(HeroClass.getClassDesc(HeroClass.WARRIOR));
         this.classDescTA.setEditable(false);
         c.gridx = 1;
         c.gridy = 2;
@@ -52,6 +54,11 @@ public class            NewHeroPanel extends JPanel implements ActionListener {
         c.gridwidth = 3;
         c.fill = GridBagConstraints.BOTH;
         this.add(this.classDescTA, c);
+    }
+
+    private void clearTF(JTextField tf) {
+        if (tf.getText().equals("Enter Hero's name") || tf.getText().equals("Enter Hero's title"))
+        tf.setText("");
     }
 
     @Override
