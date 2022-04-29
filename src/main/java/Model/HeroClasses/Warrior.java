0e371cecaa92344fd39f94050ab0eb8e8411aea6
analking228 <kronos5525@yandex.ru>
2022-04-29 13:31:05 +0300

    warrior

--- src/main/java/Model/HeroClasses/Warrior.java
+++ src/main/java/Model/HeroClasses/Warrior.java
@@ -1,4 +1,12 @@
 package Model.HeroClasses;
 
 public class        Warrior extends HeroClass {
+
+    public Warrior() {
+        this.level = 1;
+        this.xp = 0;
+        this.hp = 0;
+        this.attack = 5;
+        this.defense = 5;
+    }
 }
